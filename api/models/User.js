ed2fd965d5692b696c68299ebeac9e51bd127e2c
VERAN <xavier.veran@etu.unice.fr>
2017-01-12 12:40:07 +0100

    Last commit

--- api/models/User.js
+++ api/models/User.js
@@ -4,39 +4,47 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-var bcrypt = require('bcryptjs');
+
+ var bcrypt = require('bcryptjs');
 
 module.exports = {
 
-  attributes: {
-      email: {
-                  type: 'email',
-                  required: true,
-                  unique: true
-              },
-              password: {
-                  type: 'string',
-                  minLength: 6,
-                  required: true
-              },
-              toJSON: function() {
-                  var obj = this.toObject();
-                  delete obj.password;
-                  return obj;
-              }
+ attributes: {
+         email: {
+             type: 'email',
+             required: true,
+             unique: true
+         },
+         password: {
+             type: 'string',
+             minLength: 6,
+             required: true
+         },
+         prenom: {
+                      type: 'string',
+                      minLength: 3,
+                      required: false
           },
-          beforeCreate: function(user, cb) {
-              bcrypt.genSalt(10, function(err, salt) {
-                  bcrypt.hash(user.password, salt, function(err, hash) {
-                      if (err) {
-                          console.log(err);
-                          cb(err);
-                      } else {
-                          user.password = hash;
-                          cb();
-                      }
-                  });
-              });
-  }
+
+         toJSON: function() {
+             var obj = this.toObject();
+             delete obj.password;
+             return obj;
+         }
+     },
+     beforeCreate: function(user, cb) {
+     console.log('ok');
+         bcrypt.genSalt(10, function(err, salt) {
+             bcrypt.hash(user.password, salt, function(err, hash) {
+                 if (err) {
+                     console.log(err);
+                     cb(err);
+                 } else {
+                     user.password = hash;
+                     cb();
+                 }
+             });
+         });
+     }
 };
 
